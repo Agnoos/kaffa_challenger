646f22aa6accc0ed0ef95db7bb8244c549368211
Lucas Ricardo <lucasricardolemes@gmail.com>
2020-08-11 17:24:47 -0300

    Add images and MER page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './styles.css'
+import kaffa from '../../assets/imagens/kaffa.png'
+import github from '../../assets/imagens/github.png'
 function Landing() {
 
     return (
         <div className="navbar">
+            <ul className="nav-itens logo-align">
+                <li><img className="logo" src={kaffa} alt="" /></li>
+            </ul>
             <ul className="nav-itens">
                 <li className="nav-iten"><Link to="/ValidateCnpj" className="Button">CNPJs</Link></li>
                 <li className="nav-iten"><Link to="/RetangleIntersection" className="Button">Intersection</Link></li>
                 <li className="nav-iten"><Link to="/RestClientService" className="Button">RestClient</Link></li>
                 <li className="nav-iten"><Link to="/Mer" className="Button">MER</Link></li>
                 <li className="nav-iten"><Link to="/Ux" className="Button">UX</Link></li>
-                {/* <li className="nav-iten"><Link to="/ValidateCnpj" className="Button">Repositório</Link></li> */}
+            </ul>
+            <ul className="nav-itens logo-align">
+                <li><img className="logo" src={github} alt="" /></li>
             </ul>
         </div>
     )
--- src/pages/Mer/index.tsx
+++ src/pages/Mer/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import './styles.css'
+
+import mer from '../../assets/imagens/MER.png'
 function Mer() {
 
     return (
-        <div className="">
-           doiasjdias
+        <div className="mer">
+            <div>
+                <img className="img" src={mer} alt="" />
+            </div>
+            <a href="https://app.lucidchart.com/invitations/accept/31f382a8-25ce-4d26-8563-df5593b96e94" target="_blank"> Acessar Fluxo Online</a>
         </div>
+
+
     )
 }
 
